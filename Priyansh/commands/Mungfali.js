fe7fabcf94170a745ac80f68e23ed0bc53691141
Nainnvn <botfb84@gmail.com>
2024-08-13 22:32:51 +0530

    Update Mungfali.js

--- Priyansh/commands/Mungfali.js
+++ Priyansh/commands/Mungfali.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("mungfali")==0 || event.body.indexOf("Mungfali")==0 || event.body.indexOf("MUNGFALI")==0 || event.body.indexOf(".mungfali")==0) {
 		var msg = {
-				body: "💝 YE LO BBY MUNGFALI KHAO 🥜\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝗔𝗮𝗿𝘆𝗮𝗻❤️-)",
+				body: "💝 YE LO BBY MUNGFALI KHAO 🥜\n\n(─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/mungfali.jpg`)
 			}
