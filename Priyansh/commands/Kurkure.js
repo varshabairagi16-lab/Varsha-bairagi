1d4521862978b466f34f0a96e55249c6d2a2184d
Nainnvn <botfb84@gmail.com>
2024-08-13 22:30:49 +0530

    Update Kurkure.js

--- Priyansh/commands/Kurkure.js
+++ Priyansh/commands/Kurkure.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("KURKURE")==0 || event.body.indexOf("Kurkure")==0 || event.body.indexOf("kurkure")==0 || event.body.indexOf(".kurkure")==0) {
 		var msg = {
-				body: "💝 YE LO BBY KURKURE KHAO 🫓\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝗔𝗮𝗿𝘆𝗮𝗻❤️-)",
+				body: "💝 YE LO BBY KURKURE KHAO 🫓\n\n(─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/Kurkure.jpg`)
 			}
