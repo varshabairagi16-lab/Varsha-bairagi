12437632d236021845532d84f2aff930e550522d
Nainnvn <botfb84@gmail.com>
2024-08-13 22:28:00 +0530

    Update Jalebi.js

--- Priyansh/commands/Jalebi.js
+++ Priyansh/commands/Jalebi.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("JALEBI")==0 || event.body.indexOf("Jalebi")==0 || event.body.indexOf("jalebi")==0 || event.body.indexOf(".jalebi")==0) {
 		var msg = {
-				body: "💝 YE LO BBY JALEBI KHAO 🥨\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝗔𝗮𝗿𝘆𝗮𝗻❤️-)",
+				body: "💝 YE LO BBY JALEBI KHAO 🥨\n\n(─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/Jalebi.jpg`)
 			}
