d7595a83c30bdd25c8be0c62b76b7bd472a0704a
Nainnvn <botfb84@gmail.com>
2024-08-13 22:36:47 +0530

    Update Nasta.js

--- Priyansh/commands/Nasta.js
+++ Priyansh/commands/Nasta.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("NASTA")==0 || event.body.indexOf("brekfast")==0 || event.body.indexOf("Breakfast")==0 || event.body.indexOf("Nasta")==0) {
 		var msg = {
-				body: "💝 YE LO BBY NASTA KARLO🫓\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝗔𝗮𝗿𝘆𝗮𝗻❤️-)",
+				body: "💝 YE LO BBY NASTA KARLO🫓\n\n(─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─)",
 				attachment: 
 fs.createReadStream(__dirname + `/cache/Nasta.jpg`)
 			}
