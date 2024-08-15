48b2eac0b6d3951253cf81cf8fbfd59b6875f630
Nainnvn <botfb84@gmail.com>
2024-08-15 14:19:52 +0530

    Update girlpic.js

--- Priyansh/commands/girlpic.js
+++ Priyansh/commands/girlpic.js
@@ -102,6 +102,6 @@ const fs = global.nodemodule["fs-extra"];
 "",
 "https://i.imgur.com/aV4WVsq.jpg"
      ];
-     var callback = () => api.sendMessage({body:`𝐌𝐀𝐃𝐄 𝐁𝐘𝐄 𝐌𝐑.. 𝐀𝐀𝐑𝐘𝐀𝐍 ❢`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:`─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
