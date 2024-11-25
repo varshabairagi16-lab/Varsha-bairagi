4c3fbfcb000cae8b6772df7df4c949fbab75af74
Nainnvn <botfb84@gmail.com>
2024-11-25 12:11:52 +0530

    Update biskit.js

--- Priyansh/commands/biskit.js
+++ Priyansh/commands/biskit.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("biskit")==0 || event.body.indexOf("Biskit")==0 || event.body.indexOf("BISKIT")==0 || event.body.indexOf("Biskut")==0) {
 		var msg = {
-				body: "💝𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐁𝐈𝐒𝐊𝐄𝐓 𝐊𝐇𝐀𝐎🤟\n\n(-𝐌𝐚𝐝𝐞 𝐁𝐲 𝗠𝗿..𝗔𝗮𝗿𝘆𝗮𝗻❤️-)",
+				body: "💝𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐁𝐈𝐒𝐊𝐄𝐓 𝐊𝐇𝐀𝐎🤟\n\n(─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─)",
 				attachment: fs.createReadStream(__dirname + `/cache/biskit.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
