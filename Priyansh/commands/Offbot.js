3f07eb52e7b08a8cd99041609e59d39605bf6a7a
Nvn6 <148467720+Nvn6@users.noreply.github.com>
2025-01-16 19:12:04 +0530

    Add files via upload

---
+++ Priyansh/commands/Offbot.js
@@ -0,0 +1,14 @@
+module.exports.config = {
+	name: "offbot",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	description: "turn the bot off",
+	commandCategory: "system",
+	cooldowns: 0
+        };
+module.exports.run = ({event, api}) =>{
+    const permission = ["100078833520452", "100078833520452"];
+  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For ─━━◉❖𝗝𝗨𝗟𝗠𝗜≛𝗝𝗔𝗔𝗧❖◉━━─", event.threadID, event.messageID);
+  api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
+}
