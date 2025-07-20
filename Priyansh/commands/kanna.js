0351e7723a6085f136c735df9f275e5e60a4b189
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-07-20 19:03:58 +0530

    Update kanna.js
    Signed-off-by: Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>

--- Priyansh/commands/kanna.js
+++ Priyansh/commands/kanna.js
@@ -1,27 +1,39 @@
 module.exports.config = {
-  name: "kanna",
+  name: "pani",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "See pictures of baby dragons",
-  commandCategory: "ramdom-images",
-  usages: "kanna",
-  cooldowns: 5
+  description: "Random ảnh ayaka",
+  commandCategory: "Random-IMG",
+  usages: "ayaka",
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+    
 };
 
-module.exports.run = async function({ api, event }) {
-  const axios = require('axios');
-  const request = require('request');
-  const fs = require("fs");
-  axios.get('https://apikanna.khoahoang2.repl.co').then(res => {
-  let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
-  let count = res.data.count;
-  let callback = function () {
-          api.sendMessage({
-            body: `⚡️This is Kanna <3\n⚡️Number of photos available: ${count} Photo`,
-            attachment: fs.createReadStream(__dirname + `/cache/kanna.${ext}`)
-          }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/kanna.${ext}`), event.messageID);
-        };
-        request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/kanna.${ext}`)).on("close", callback);
-      })
-}
+module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+  var link = [
+  "https://i.imgur.com/uXWLBeC.jpeg",
+    "https://i.imgur.com/7Dc9GrN.jpeg",
+    eg",
+           "https://i.imgur.com/6iiwTNP.jpeg",
+  ];
+  var max = Math.floor(Math.random() * 6);  
+var min = Math.floor(Math.random() * 2);
+  var data = await Currencies.getData(event.senderID);
+  var exp =  data.exp;
+  var money = data.money
+      if(money < 0) api.sendMessage("Do you need $100 to see the photo?",event.threadID,event.messageID)
+          else {
+   Currencies.setData(event.senderID, options = {money: money - 0})
+   var callback = () => api.sendMessage({body:`ye le dub ja daffar ${link.length}\n-100 usd!`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min))).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
+     }
+   };
